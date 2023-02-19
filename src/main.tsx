02ee0a325d2ef9e86287539df8712452a32897ae
Akisan <51326806+Akisan98@users.noreply.github.com>
2023-02-19 20:09:37 +0100

    FEAT: FullPageLoader

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import HomePage from './features/home/page/homePage.lazy'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <HomePage/>
   </React.StrictMode>,
 )
