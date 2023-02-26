df7c88756294f42eeece90fa99284e9e1c5bee37
Akisan <51326806+Akisan98@users.noreply.github.com>
2023-02-26 12:56:05 +0100

    Fix casing
    * Update and rename homePage.tsx to HomePage.tsx
    
    * Rename homePage.lazy.tsx to HomePage.lazy.tsx
    
    * Rename homePage.module.scss to HomePage.module.scss

--- src/features/home/page/HomePage.tsx
+++ src/features/home/page/HomePage.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Hero from '../components/Hero/Hero';
-import styles from './homePage.module.scss';
+import styles from './HomePage.module.scss';
 
 const HomePage: FC = () => (
   <div className={styles.HomePage} data-testid="HomePage">
